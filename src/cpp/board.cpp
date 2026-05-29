2058b4131ebb75e50e9741cf6dc2b86d9a728c66
Nabil Imron <nabilimron12@gmail.com>
2026-05-29 16:06:27 +0800

    Menambahkan fungsi display

--- src/cpp/board.cpp
+++ src/cpp/board.cpp
@@ -7,3 +7,20 @@ board::board(){
         for (int j = 0; j < size; j++)
             grid[i][j] = empty;
 }
+
+void board::display() {
+    cout << "  ";
+    for (int j = 0; j < size; j++)
+        cout << j << " ";
+    cout << endl;
+
+    for (int i = 0; i < size; i++) {
+        cout << i << " ";
+        for (int j = 0; j < size; j++) {
+            if (grid[i][j] == empty) cout << ". ";
+            else if (grid[i][j] == black) cout << "B ";
+            else cout << "W ";
+        }
+        cout << endl;
+    }
+}
