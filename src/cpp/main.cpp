93842da5e4842dd8bb1478206f817db79acfa789
Nabil Imron <250210501047@student.unm.ac.id>
2026-05-29 23:06:08 +0800

    add main game loop with two-player turn and stone placement

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 
 int main(){
+        board board;
+    int row, col;
+    cell currentPlayer = black;
+
+    cout << "=== Permainan Go 9x9 ===" << endl;
+
+    while (true) {
+        board.display();
+        cout << (currentPlayer == black ? "Hitam" : "Putih") << " - masukkan baris dan kolom: ";
+        cin >> row >> col;
+
+        if (board.stone(row, col, currentPlayer)) {
+            currentPlayer = (currentPlayer == black) ? white : black;
+        }
+    }
 
     return 0;
 }
